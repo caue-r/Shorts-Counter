5cd7994a67ec4f91f7f24163a921c851d48fd3bc
Cauê Reis <cauereis846@gmail.com>
2025-08-07 00:14:26 -0300

    Update background.js

--- background.js
+++ background.js
@@ -3,7 +3,6 @@ const LOTE_SIZE = 10;
 let counter = 0;
 let videosSeen = {};
 
-// Log inicial
 console.log('[Shorts Counter] Service Worker carregado!');
 
 // Carrega dados do storage ao iniciar
@@ -13,28 +12,28 @@ chrome.storage.local.get(['shortCounter', 'videosSeen'], (data) => {
   console.log(`[Shorts Counter] Dados carregados: contador = ${counter}, vídeos no lote: ${Object.keys(videosSeen).length}`);
 });
 
-// Recebe mensagem do content script para contar novo vídeo
 chrome.runtime.onMessage.addListener((message) => {
   if (message.type === "INCREMENT_COUNTER") {
     console.log(`[Shorts Counter] Mensagem recebida: INCREMENT_COUNTER para o vídeo ${message.videoId}`);
 
     if (!videosSeen[message.videoId]) {
       videosSeen[message.videoId] = true;
-      console.log(`[Shorts Counter] Novo vídeo registrado no lote: ${message.videoId} (total no lote: ${Object.keys(videosSeen).length})`);
+      counter++;  // Agora soma 1 a cada vídeo novo
+      console.log(`[Shorts Counter] Novo vídeo registrado: ${message.videoId} (total no lote: ${Object.keys(videosSeen).length}), contador = ${counter}`);
 
-      // Se atingiu o lote, soma ao contador e zera
+      // Se atingiu o lote, zera só a lista de vídeos
       if (Object.keys(videosSeen).length >= LOTE_SIZE) {
-        counter += LOTE_SIZE;
         videosSeen = {};
         chrome.storage.local.set({ shortCounter: counter, videosSeen }, () => {
-          console.log(`[Shorts Counter] Lote de ${LOTE_SIZE} fechado! Contador incrementado para ${counter}. Lote reiniciado.`);
+          console.log(`[Shorts Counter] Lote de ${LOTE_SIZE} fechado! Lote reiniciado. Contador permanece: ${counter}`);
         });
         chrome.action.setBadgeText({ text: counter.toString() });
       } else {
-        // Salva o estado intermediário (a cada novo vídeo visto)
+        // Salva o estado intermediário (contador + vídeos vistos)
         chrome.storage.local.set({ shortCounter: counter, videosSeen }, () => {
           console.log(`[Shorts Counter] Estado intermediário salvo: contador = ${counter}, vídeos no lote = ${Object.keys(videosSeen).length}`);
         });
+        chrome.action.setBadgeText({ text: counter.toString() });
       }
     } else {
       console.log(`[Shorts Counter] Vídeo ${message.videoId} já registrado neste lote. Nada feito.`);
